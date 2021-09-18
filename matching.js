0d75c04762791628888cee2452387d17d0ca4dc5
Hyun Min Kim <44655955+gomgom03@users.noreply.github.com>
2021-09-18 11:07:26 -0500

    matching+wikisearch

--- matching.js
+++ matching.js
@@ -1,16 +1,24 @@
 const fetch = require("node-fetch");
 const fs = require('fs');
 const {api_key} = JSON.parse(fs.readFileSync('keys.json', {encoding:'utf8', flag:'r'}));
-const wiki_CX = "17125c511a5a54b01"
+const wiki_CX = "17125c511a5a54b01";
+const wikisearch = require("./wikisearch.js");
 
 async function getData(query){
     console.log(`https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${wiki_CX}&q=sunfish%20wikipedia`);
     const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${wiki_CX}&q=${encodeURIComponent(query)}`);
     const data = await response.json();
-    console.log(data.items[0].link.split("/").pop());
+    qStr = data.items[0].link.split("/").pop();
+    return qStr
     //pass above in wiki
     
     //console.log(Object.keys(data));
 }
 
-getData("moroccan rice pilaf")
+getData("moroccan rice pilaf").then((x)=>{
+    console.log(x);
+    (async ()=>{
+        foodSummary = await wikisearch(x);
+        console.log(foodSummary);
+    })();
+})
---
+++ wikisearch.js
@@ -0,0 +1,20 @@
+const wiki = require('wikipedia');
+
+module.exports = async function getWikiPage(qStr) {
+	try {
+		const page = await wiki.page(qStr);
+		//console.log(page);
+		//page.content().then((x)=>{console.log(x.split(" "))});
+		//Response of type @Page object
+		const summary = await page.summary();
+		//console.log(Object.keys(summary));
+        console.log("LOGGED", summary);
+        return summary.extract
+		//Res.ponse of type @wikiSummary - contains the intro and the main image
+	} catch (error) {
+		//console.log(error);
+		//=> Typeof wikiError
+        console.log("LOGGED2", error);
+        return "";
+	}
+}
