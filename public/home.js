01cfced0dece44d3c6175893131a154c50aa0e96
eyangx <emilyx_yang@live.com>
2021-09-19 05:11:16 -0500

    style and frontend improvements

--- public/home.js
+++ public/home.js
@@ -2,11 +2,15 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js'
 import { db } from '/firebase.js';
 
-const labelsTable = document.getElementById("labels-table");
-const labelsTableHead = document.getElementById("labels-table-head");
 const freeList = document.getElementById("free-list");
 const freeInput = document.getElementById("free-input");
 const freeSubmit = document.getElementById("free-submit");
+const welcomeMessage = document.getElementById("welcome-message");
+const labelsList = document.getElementById("rstr-labels-list-container");
+
+const mainForm = document.getElementById("main-form");
+const nextButton = document.getElementById("next-button");
+const backButton = document.getElementById("back-button");
 const saveButton = document.getElementById("save-button");
 
 const labels = [
@@ -30,8 +34,8 @@ const labelsNames = {
 	"shellfish": "Shellfish",
 	"soy": "Soy",
 	"tree-nuts": "Tree Nuts",
-	"vegan": "Vegan",
-	"vegetarian": "Vegetarian",
+	"vegan": "Vegan Food",
+	"vegetarian": "Vegetarian Food",
 };
 
 let choices = {
@@ -48,51 +52,50 @@ function clearElem(elem) {
 	}
 }
 
-function renderTable() {
-	const clonedHead = labelsTableHead.cloneNode(true);
-	clearElem(labelsTable);
-	labelsTable.appendChild(clonedHead);
-
+function renderLabelsList() {
 	for (const label of labels) {
-		const tr = document.createElement("tr");
-		const labelTd = document.createElement("td");
-		labelTd.appendChild(document.createTextNode(labelsNames[label]));
-		const rstrTd = document.createElement("td");
-		const rstrCheckbox = document.createElement("input");
-		rstrCheckbox.setAttribute("type", "checkbox");
-		if (choices.rstr.has(label)) rstrCheckbox.setAttribute("checked", true);
-		rstrCheckbox.addEventListener("change", () => {
-			const checked = rstrCheckbox.checked;
-			if (checked) choices.rstr.add(label);
-			else choices.rstr.delete(label);
+		const checkbox = document.createElement("input");
+		checkbox.setAttribute("type", "checkbox");
+		checkbox.classList.add("btn-check");
+		checkbox.id = `rstr-checkbox-${label}`;
+		const button = document.createElement("label");
+		button.setAttribute("for", checkbox.id);
+		button.appendChild(document.createTextNode(labelsNames[label]));
+		button.classList.add("btn", "label-check");
+		if (choices.rstr.has(label)) button.classList.add("btn-danger");
+		else button.classList.add("btn-secondary");
+		checkbox.addEventListener("change", () => {
+			if (checkbox.checked) {
+				choices.rstr.add(label);
+				button.classList.add("btn-danger");
+				button.classList.remove("btn-secondary");
+			}
+			else {
+				choices.rstr.delete(label);
+				button.classList.remove("btn-danger");
+				button.classList.add("btn-secondary");
+			}
 		});
-		rstrTd.appendChild(rstrCheckbox);
-		const prefTd = document.createElement("td");
-		const prefCheckbox = document.createElement("input");
-		prefCheckbox.setAttribute("type", "checkbox");
-		if (choices.pref.has(label)) prefCheckbox.setAttribute("checked", true);
-		prefCheckbox.addEventListener("change", () => {
-			const checked = prefCheckbox.checked;
-			if (checked) choices.pref.add(label);
-			else choices.pref.delete(label);
-		});
-		prefTd.appendChild(prefCheckbox);
-		tr.appendChild(labelTd);
-		tr.appendChild(rstrTd);
-		tr.appendChild(prefTd);
-		labelsTable.appendChild(tr);
+		labelsList.appendChild(checkbox);
+		labelsList.appendChild(button);
 	}
 }
 
 function renderFreeItem(freeItem) {
 	const itemNode = document.createElement("div");
-	itemNode.appendChild(document.createTextNode(freeItem));
+	itemNode.classList.add("btn-group", "free-item");
+	const labelButton = document.createElement("button");
+	labelButton.appendChild(document.createTextNode(freeItem));
+	labelButton.classList.add("btn", "btn-outline-secondary");
+	labelButton.setAttribute("disabled", true);
 	const removeButton = document.createElement("button");
-	removeButton.appendChild(document.createTextNode("Remove"));
+	removeButton.classList.add("btn", "btn-danger", "free-item-remove");
+	removeButton.appendChild(document.createTextNode("✖"));
 	removeButton.addEventListener("click", () => {
 		itemNode.remove();
 		choices.free = choices.free.filter(e => e != freeItem);
 	});
+	itemNode.appendChild(labelButton);
 	itemNode.appendChild(removeButton);
 	freeList.appendChild(itemNode);
 }
@@ -127,13 +130,22 @@ function start() {
 	const auth = getAuth();
 	onAuthStateChanged(auth, async (user) => {
 		if (user) {
+			welcomeMessage.textContent = `Welcome, ${user.displayName.split(" ")[0]}!`;
 			const userDocRef = doc(db, 'users', user.uid);
 			const userDoc = (await getDoc(userDocRef)).data();
 			choices.free = userDoc?.free ?? [];
 			choices.rstr = new Set(userDoc?.rstr ?? []);
 			choices.pref = new Set(userDoc?.pref ?? []);
-			renderTable();
+			renderLabelsList();
 			renderFreeList();
+			nextButton.addEventListener('click', () => {
+				mainForm.classList.remove("main-form-focus-first");
+				mainForm.classList.add("main-form-focus-second");
+			});
+			backButton.addEventListener('click', () => {
+				mainForm.classList.add("main-form-focus-first");
+				mainForm.classList.remove("main-form-focus-second");
+			});
 			saveButton.addEventListener('click', async () => {
 				await setDoc(userDocRef, {
 					displayName: userDoc.displayName,
@@ -143,6 +155,7 @@ function start() {
 					rstr: [...choices.rstr],
 					pref: [...choices.pref],
 				});
+				alert("Saved successfully. We will be texting you!");
 			});
 		}
 		else {
